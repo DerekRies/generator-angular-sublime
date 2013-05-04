17f3afb76a1b4c1e30ca906c9573b27428c977a8
Derek Ries <influenztial@gmail.com>
2013-05-03 18:29:03 -0700

    now opens files when they are finished being created. Still need to complete communication with the initial app scaffolding

---
+++ README.md
@@ -0,0 +1,5 @@
+## Generator-Angular-Sublime
+
+This is a Sublime Text 2 plugin that supports the features provided by the generator-angular yeoman generator. Instead of having to tab out of sublime, open up a terminal, then enter in exhaustingly long "yo angular:route myroute", then tab back into sublime, and finally open up the files that were created; just open up the command palette, type route, enter a name, and let the plugin do the rest.
+
+This is just something I hacked together because I was getting a little fatigued by the experience described above. It only supports the scaffolding commands once the app has already been created at the moment. Should have the ability to create new angular apps from sublime soon as well.
--- generator-angular.py
+++ generator-angular.py
@@ -3,8 +3,11 @@ import sublime_plugin
 import subprocess
 import os
 import errno
-import re
 import threading
+import time
+
+# from Queue import Queue, Empty
+
 
 def mkdir_p(path):
     try:
@@ -12,8 +15,8 @@ def mkdir_p(path):
     except OSError as exc:
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
-        else: raise
-
+        else:
+            raise
 
 
 class AngularCommand(sublime_plugin.WindowCommand):
@@ -22,14 +25,15 @@ class AngularCommand(sublime_plugin.WindowCommand):
     and the folder name
     """
     def run(self):
+        # sublime.error_message("haha shits broke son")
         user_path = os.path.expanduser("~")
         if len(self.window.folders()) == 0:
-            self.window.show_input_panel("Application Name:",user_path+"/",self.on_done, None, None)
+            self.window.show_input_panel("Application Name:", user_path+"/", self.on_done, None, None)
         else:
             self.run_yo(self.window.folders()[0])
 
     def on_done(self, user_input):
-        if os.path.isdir(user_input) == False:
+        if os.path.isdir(user_input) is False:
             print("Creating dir")
             mkdir_p(user_input)
             os.chmod(user_input, 0777)
@@ -37,6 +41,7 @@ class AngularCommand(sublime_plugin.WindowCommand):
 
     def run_yo(self, path):
         thread = AngularAppGenerator(path)
+        thread.daemon = True
         thread.start()
         # print(os.getcwd())
 
@@ -49,16 +54,16 @@ class AngularAppGenerator(threading.Thread):
     def run(self):
         os.chdir(self.path)
         # output = subprocess.Popen(["ls"], stdout=subprocess.PIPE).communicate()[0]
-        output = subprocess.Popen(["yo","angular","--minsafe"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        output = subprocess.Popen(["yo", "angular", "--minsafe"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         # response = output.stdout.read()
         print(output.communicate(input="Y\n"))
-        # print(output.communicate())
-        # output.communicate('Y')
-        # print(output.communicate())
+        """
+        Okay heres what needs to happen here
+        While the program is running:
+            We need to wait on a response from the yo angular command
+            Then we need to response accordingly
+        """
         print("thread done")
-
-
-
 
 
 class ThreadedTaskRunner(threading.Thread):
@@ -71,16 +76,30 @@ class ThreadedTaskRunner(threading.Thread):
         os.chdir(self.path)
         output = subprocess.Popen(self.cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         print(output.communicate()[0])
+        file_path = self.path + "/app/"
+        files = []
+        print(self.cmd[1])
+        if self.cmd[1] == "angular:route":
+            files.append(file_path + "views/{0}.html".format(self.cmd[2]))
+            files.append(file_path + "scripts/controllers/{0}.js".format(self.cmd[2]))
+        elif self.cmd[1] == "angular:controller":
+            files.append(file_path + "scripts/controllers/{0}.js".format(self.cmd[2]))
+        elif self.cmd[1] == "angular:service":
+            files.append(file_path + "scripts/services/{0}.js".format(self.cmd[2]))
+        elif self.cmd[1] == "angular:directive":
+            files.append(file_path + "scripts/directives/{0}.js".format(self.cmd[2]))
+        elif self.cmd[1] == "angular:filter":
+            files.append(file_path + "scripts/filters/{0}.js".format(self.cmd[2]))
+
+        for f in files:
+            sublime.windows()[0].open_file(f)
         print("Angular Task Should Have Run")
-
-
-
-
 
 
 class AngularRoute(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.show_input_panel("Route Name:","", self.on_done, None, None)
+        self.window.show_input_panel("Route Name:", "", self.on_done, None, None)
+
     def on_done(self, text):
         thread = ThreadedTaskRunner(self.window.folders()[0], ["yo", "angular:route", text, "--minsafe"])
         thread.start()
@@ -88,7 +107,8 @@ class AngularRoute(sublime_plugin.WindowCommand):
 
 class AngularController(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.show_input_panel("Controller Name:","", self.on_done, None, None)
+        self.window.show_input_panel("Controller Name:", "", self.on_done, None, None)
+
     def on_done(self, text):
         thread = ThreadedTaskRunner(self.window.folders()[0], ["yo", "angular:controller", text, "--minsafe"])
         thread.start()
@@ -96,7 +116,8 @@ class AngularController(sublime_plugin.WindowCommand):
 
 class AngularDirective(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.show_input_panel("Directive Name:","", self.on_done, None, None)
+        self.window.show_input_panel("Directive Name:", "", self.on_done, None, None)
+
     def on_done(self, text):
         thread = ThreadedTaskRunner(self.window.folders()[0], ["yo", "angular:directive", text, "--minsafe"])
         thread.start()
@@ -104,7 +125,8 @@ class AngularDirective(sublime_plugin.WindowCommand):
 
 class AngularFilter(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.show_input_panel("Filter Name:","", self.on_done, None, None)
+        self.window.show_input_panel("Filter Name:", "", self.on_done, None, None)
+
     def on_done(self, text):
         thread = ThreadedTaskRunner(self.window.folders()[0], ["yo", "angular:filter", text, "--minsafe"])
         thread.start()
@@ -112,7 +134,8 @@ class AngularFilter(sublime_plugin.WindowCommand):
 
 class AngularService(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.show_input_panel("Service Name:","", self.on_done, None, None)
+        self.window.show_input_panel("Service Name:", "", self.on_done, None, None)
+
     def on_done(self, text):
         thread = ThreadedTaskRunner(self.window.folders()[0], ["yo", "angular:service", text, "--minsafe"])
         thread.start()
@@ -120,9 +143,8 @@ class AngularService(sublime_plugin.WindowCommand):
 
 class AngularView(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.show_input_panel("View Name:","", self.on_done, None, None)
+        self.window.show_input_panel("View Name:", "", self.on_done, None, None)
+
     def on_done(self, text):
         thread = ThreadedTaskRunner(self.window.folders()[0], ["yo", "angular:view", text, "--minsafe"])
         thread.start()
-
-
